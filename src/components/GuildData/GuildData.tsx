206637c3dd059f79cba075fddd2a74abc4c641ec
Chris Tieman <chris.tieman@moserit.com>
2020-03-13 23:25:12 -0400

    Removed the interval from the UI side for sockets

--- src/components/GuildData/GuildData.tsx
+++ src/components/GuildData/GuildData.tsx
@@ -9,30 +9,20 @@ interface IGuildDataProps {
 
 const URL = process.env.NODE_ENV === 'development' ? 'http://localhost:4444' : '';
 
-const connect = () => {
-  const socket = socketIOClient(URL);
-  return socket;
-};
-
 export const GuildData = (props: IGuildDataProps) => {
   const [data, setData] = useState<IGuildResponseData | null>(null);
 
   useEffect(() => {
-    const socket = connect();
+    const socket = socketIOClient(URL);
+
     socket.on('response:guildData', setData);
 
-    socket.emit('request:guildData');
-    const intvl = setInterval(() => socket.emit('request:guildData'), 10 * 1000);
-
     return () => {
-      clearInterval(intvl);
       socket.close();
     };
   }, []);
 
-  if (!data) {
-    return props.loading;
-  }
+  if (!data) return props.loading;
 
   return props.children(data);
 };
