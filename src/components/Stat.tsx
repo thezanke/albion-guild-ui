fc53c5a3cd24db8d985acbef176c0d101d406893
Chris Tieman <chris.tieman@moserit.com>
2020-03-13 23:25:39 -0400

    Created Stat component

---
+++ src/components/Stat.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import theme from '../styles/theme';
+
+const StyledStat = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+
+  .Stat__label {
+    font-weight: 700;
+    margin: 0 0 5px;
+  }
+
+  .Stat__number {
+  }
+`;
+
+const Stat = ({ label, value, ...rest }: any) => {
+  return (
+    <StyledStat {...rest}>
+      <div className="Stat__label">{label}</div>
+      <div className="Stat__number">{value}</div>
+    </StyledStat>
+  );
+};
+
+export default Stat;
